d054078c53bd8b568ee3f14362f703eacd1717cd
felovilches <felovilches@gmail.com>
2016-07-24 06:07:41 -0400

    reduje el codigo de Fenwick tree

--- F Fence the vegetables fail.cpp
+++ F Fence the vegetables fail.cpp
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// BIT with range updates, inspired by Petr Mitrichev
-struct BIT {
-    int n;
-    vector<int> slope;
-    vector<int> intercept;
-    // BIT can be thought of as having entries f[1], ..., f[n]
-    // which are 0-initialized
-    BIT(int n): n(n), slope(n+1), intercept(n+1) {}
-    // returns f[1] + ... + f[idx-1]
-    // precondition idx <= n+1
-    int query(int idx) {
-        int m = 0, b = 0;
-        for (int i = idx-1; i > 0; i -= i&-i) {
-            m += slope[i];
-            b += intercept[i];
-        }
-        return m*idx + b;
-    }
-    // adds amt to f[i] for i in [idx1, idx2)
-    // precondition 1 <= idx1 <= idx2 <= n+1 (you can't update element 0)
-    void update(int idx1, int idx2, int amt) {
-        for (int i = idx1; i <= n; i += i&-i) {
-            slope[i] += amt;
-            intercept[i] -= idx1*amt;
-        }
-        for (int i = idx2; i <= n; i += i&-i) {
-            slope[i] -= amt;
-            intercept[i] += idx2*amt;
-        }
-    }
-};
+// A[] is the original array
+// int A[] (no se usa)
+
+// the fenwick tree maintaining the diffs initialized with 0
+int diff_array[900000];
+int fenwick_n;
+ 
+// Add v to A[p]
+void update(int p, int v){
+	p++;
+	for (; p <= fenwick_n; p += p&(-p))
+		diff_array[p] += v;
+}
+
+// Add v to A[a...b] 
+void update(int a, int b, int v){
+	update(a, v);
+	update(b + 1, -v);
+}
+
+// Return A[b]	 
+int query(int b){
+	b++;   
+	int sum = 0;
+	for(; b > 0; b -= b&(-b))
+		sum += diff_array[b];
+	return sum/* + A[b] ... array original, pero este es siempre 0*/;
+}
 
 class punto{
 public:
 	int x, y, valor;
 	punto(int v) : valor(v){
-		cin>>x>>y;
+		scanf("%d %d", &x, &y);
 	}
 };
 
@@ -45,7 +42,7 @@ int P, V;
 
 int main(){
 	
-	while(cin>>P>>V){
+	while(scanf("%d %d", &P, &V) == 2){
 
 		vector<punto> puntos;
 		for(int i=0; i<P; i++) puntos.push_back(punto(i+1));
@@ -76,9 +73,9 @@ int main(){
 			else return p1.x < p2.x;
 		});
 
-		BIT fenwick(y_comprimido + 7);
-
 		long long int valor = 0;
+		fenwick_n = y_comprimido+7;
+		memset(diff_array, 0, sizeof(diff_array)); 
 
 		for(int i=0; i<puntos.size(); i++){
 
@@ -90,7 +87,7 @@ int main(){
 
 				// Se tiene un intervalo p1 a p2.
 				// Incrementar +1 al intervalo.
-				fenwick.update(p1.y, p2.y, 1);
+				update(p1.y, p2.y, 1);
 
 			} else {
 
@@ -100,13 +97,9 @@ int main(){
 				// y se cuenta cuantas veces intersecta el poligono, si es un numero impar,
 				// entonces esta dentro. (Esto es lo mismo que prender y apagar el rango
 				// del fenwick tree, 0=par/fuera, 1=impar/dentro)
-				int intersecciones = fenwick.query(p1.y+1) - fenwick.query(p1.y); 
-
-				if(intersecciones % 2 == 0) valor += p1.valor;
-						
+				if(query(p1.y+1)% 2 == 0) valor += p1.valor;						
 			}
 		}
-
 		printf("%lld\n", valor);
 	}
 	return 0;
